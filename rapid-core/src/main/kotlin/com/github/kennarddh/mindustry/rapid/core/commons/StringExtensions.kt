9a62482231e659eddc12b2562d27e69e73737bf5
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-03-05 16:58:17 +0700

    feat: Remove unused string extensions

--- rapid-core/src/main/kotlin/com/github/kennarddh/mindustry/rapid/core/commons/StringExtensions.kt
+++
@@ -1,8 +0,0 @@
-package com.github.kennarddh.mindustry.rapid.core.commons
-
-import arc.util.Strings
-
-fun String.stripColors(): String = Strings.stripColors(this)
-
-fun String.stripGlyphs(): String = Strings.stripGlyphs(this)
-
